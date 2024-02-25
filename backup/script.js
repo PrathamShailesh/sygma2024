05b19f0911ca05c889d59906c47a47671c602d07
nithin <bhandary0010@gmail.com>
2024-02-25 12:25:08 +0530

    v2 updated

---
+++ backup/script.js
@@ -0,0 +1,135 @@
+//   humburger menu js 
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+
+    mobileMenuIcon.addEventListener('click', function () {
+        navLinks.style.display = (navLinks.style.display === 'flex') ? 'none' : 'flex';
+    });
+});
+// Slide cards automatically
+document.addEventListener('DOMContentLoaded', () => {
+  
+  //------ Slider Begin
+    const CaroS = document.querySelector('.Carousel-slider');
+    const CaroSlider = new MicroSlider(CaroS, { indicators: true, indicatorText: '' });
+    const hammer = new Hammer(CaroS);
+    const CaroSTimer = 2000;
+    let CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+    
+  //------- Mouseenter Event
+    CaroS.onmouseenter = function(e) {
+        clearInterval(CaroAutoplay); 
+        console.log(e.type + ' mouse detected');
+    }
+  
+  //----- Mouseleave Event
+    CaroS.onmouseleave = function(e) {
+        clearInterval(CaroAutoplay); 
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + ' mouse detected');
+    }
+  
+  //----- Mouseclick Event
+    CaroS.onclick = function(e) {
+        clearInterval(CaroAutoplay); 
+        console.log(e.type + ' mouse detected');
+    }
+  
+  //------ Gesture Tap Event
+    hammer.on('tap', function(e) {
+        clearInterval(CaroAutoplay);
+        console.log(e.type + ' gesture detected');
+    });
+  
+  //----- Gesture Swipe Event
+    hammer.on('swipe', function(e) {
+        clearInterval(CaroAutoplay); 
+        CaroAutoplay = setInterval(() => CaroSlider.next(), CaroSTimer);
+        console.log(e.type + ' gesture detected');
+    });
+
+  let slideLink = document.querySelectorAll('.slider-item');
+  if (slideLink && slideLink !== null && slideLink.length > 0){
+    slideLink.forEach( el => el.addEventListener('click', e => {
+      e.preventDefault();
+      let href = el.dataset.href;
+      let target = el.dataset.target;
+      if (href !== '#') window.open(href, target);
+    }));
+  }
+  
+  //---- Slider End
+  
+});
+
+//added from here
+document.getElementById('readMoreButton1').addEventListener('click', function() {
+  document.getElementById('overlay1').style.display = 'block';
+  openPopout();
+});
+
+document.getElementById('CloseButton1').addEventListener('click', function() {
+  document.getElementById('overlay1').style.display = 'none';
+  closePopout();
+  
+});
+document.getElementById('readMoreButton2').addEventListener('click', function() {
+  document.getElementById('overlay2').style.display = 'block';
+  openPopout();
+});
+
+document.getElementById('CloseButton2').addEventListener('click', function() {
+  document.getElementById('overlay2').style.display = 'none';
+  closePopout();
+});
+
+document.getElementById('readMoreButton3').addEventListener('click', function() {
+  document.getElementById('overlay3').style.display = 'block';
+  openPopout();
+});
+
+document.getElementById('CloseButton3').addEventListener('click', function() {
+  document.getElementById('overlay3').style.display = 'none';
+  closePopout();
+});
+document.getElementById('readMoreButton4').addEventListener('click', function() {
+  document.getElementById('overlay4').style.display = 'block';
+  openPopout();
+});
+
+document.getElementById('CloseButton4').addEventListener('click', function() {
+  document.getElementById('overlay4').style.display = 'none';
+  closePopout();
+});
+document.getElementById('readMoreButton5').addEventListener('click', function() {
+  document.getElementById('overlay5').style.display = 'block';
+  openPopout();
+});
+
+document.getElementById('CloseButton5').addEventListener('click', function() {
+  document.getElementById('overlay5').style.display = 'none';
+  closePopout();
+});
+document.getElementById('readMoreButton6').addEventListener('click', function() {
+  document.getElementById('overlay6').style.display = 'block';
+  openPopout();
+  
+});
+
+document.getElementById('CloseButton6').addEventListener('click', function() {
+  document.getElementById('overlay6').style.display = 'none';
+  closePopout();
+});
+
+// Open popout
+function openPopout() {
+  document.body.classList.add('popup-open');
+  document.querySelector('.overlay').style.display = 'block';
+}
+
+// Close popout
+function closePopout() {
+  document.body.classList.remove('popup-open');
+  document.querySelector('.overlay').style.display = 'none';
+}
